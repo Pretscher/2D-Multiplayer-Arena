1a6a82a17361b9f09e13091c6053669a040ad66d
Preacher <63048280+juliankoll@users.noreply.github.com>
2021-07-18 21:43:34 +0200

    more doc

--- Eventhandling.cpp
+++ Eventhandling.cpp
@@ -10,7 +10,7 @@ Terrain* terrain;//contains every non-moving object with collision
 
 //Viewspace: value of 0 means left/top, limit value (from viewSpaceLimits) means right/bottom
 int* cViewSpace;//current viewspace position. [0] is row (bot to top), [1] is col (left to right) 
-int* viewSpaceLimits;//maximum viewspace.
+int* viewSpaceLimits;//maximum viewspace.  [0] = left, [1] = right, [2] = top, [3] = bottom
 
 int rows, cols;//rows and cols you can see at a time, viewspace limits need to be added for worldSize
 int pathfindingAccuracy;//the higher the less accuracy (1 means every pixel is considered)
