0bdf3ff561929bfdabdc19131df28e550810ee60
Preacher <63048280+juliankoll@users.noreply.github.com>
2021-07-22 13:29:47 +0200

    Terrain cpp for style

---
+++ Terrain.cpp
@@ -0,0 +1,31 @@
+#include "Terrain.hpp"
+
+Terrain::Terrain() {
+	objectsRow = new std::vector<Rect*>();
+}
+
+void Terrain::addRect(int row, int col, int width, int height) {
+	objectsRow->push_back(new Rect(row, col, width, height));
+}
+
+void Terrain::draw() {
+	for (unsigned int i = 0; i < objectsRow->size(); i++) {
+		Rect* rect = this->objectsRow->at(i);
+		Renderer::drawRect(rect->getRow(), rect->getCol(), rect->getWidth(), rect->getHeight(), sf::Color(100, 100, 100, 100));
+	}
+}
+
+void Terrain::addCollidablesToGrid(bool** grid, int pathfindingAccuracy, int playerWidth, int playerHeight) {
+	for (int i = 0; i < objectsRow->size(); i++) {
+		Rect* rect = this->objectsRow->at(i);
+		for (int y = (rect->getRow() - playerHeight + pathfindingAccuracy) / pathfindingAccuracy; y < (rect->getRow() + rect->getHeight()) / pathfindingAccuracy; y++) {
+			for (int x = (rect->getCol() - playerWidth + pathfindingAccuracy) / pathfindingAccuracy; x < (rect->getCol() + rect->getWidth()) / pathfindingAccuracy; x++) {
+				grid[y][x] = false;
+			}
+		}
+	}
+}
+
+inline std::vector<Rect*>* Terrain::getCollidables() {
+	return objectsRow;
+}
--- Terrain.hpp
+++ Terrain.hpp
@@ -3,35 +3,11 @@
 #include "Rect.hpp"
 class Terrain {
 public:
-	Terrain() {
-		objectsRow = new std::vector<Rect*>();
-	}
-
-	void addRect(int row, int col, int width, int height) {
-		objectsRow->push_back(new Rect(row, col, width, height));
-	}
-
-	void draw() {
-		for (unsigned int i = 0; i < objectsRow->size(); i++) {
-			Rect* rect = this->objectsRow->at(i);
-			Renderer::drawRect(rect->getRow(), rect->getCol(), rect->getWidth(), rect->getHeight(), sf::Color(100, 100, 100, 100));
-		}
-	}
-
-	void addCollidablesToGrid(bool** grid, int pathfindingAccuracy, int playerWidth, int playerHeight) {
-		for (int i = 0; i < objectsRow->size(); i++) {
-			Rect* rect = this->objectsRow->at(i);
-			for (int y = (rect->getRow() - playerHeight + pathfindingAccuracy) / pathfindingAccuracy; y < (rect->getRow() + rect->getHeight()) / pathfindingAccuracy; y++) {
-				for (int x = (rect->getCol() - playerWidth + pathfindingAccuracy) / pathfindingAccuracy; x < (rect->getCol() + rect->getWidth()) / pathfindingAccuracy; x++) {
-					grid[y][x] = false;
-				}
-			}
-		}
-	}
-
-	inline std::vector<Rect*>* getCollidables() {
-		return objectsRow;
-	}
+	Terrain();
+	void addRect(int row, int col, int width, int height);
+	void draw();
+	void addCollidablesToGrid(bool** grid, int pathfindingAccuracy, int playerWidth, int playerHeight);
+	inline std::vector<Rect*>* getCollidables();
 
 private:
 	std::vector<Rect*>* objectsRow;
