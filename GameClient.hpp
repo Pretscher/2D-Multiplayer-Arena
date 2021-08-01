ee2709c657b6826e7baf79c11a547e933321d299
Preacher <63048280+juliankoll@users.noreply.github.com>
2021-08-01 12:17:14 +0200

    No inheritance anymore, just use gameclient and gameserver

--- GameClient.hpp
+++ GameClient.hpp
@@ -1,9 +1,96 @@
 #pragma once
-#include "Client.hpp"
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
 
-class GameClient: public Client {
-    using Client::Client;
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+class GameClient {
+protected:
+    int iResult;
+    SOCKET ConnectSocket;
+    char* recvbuf;
+
+    int recvbuflen;
+    std::string* lastMessage;
+    bool connected = false;
+
 public:
+    GameClient(const char* serverIP) {
+        lastMessage = nullptr;
+        WSADATA wsaData;
+        ConnectSocket = INVALID_SOCKET;
+        struct addrinfo* result = NULL,
+            * ptr = NULL,
+            hints;
+        recvbuf = new char[DEFAULT_BUFLEN];
+
+        recvbuflen = DEFAULT_BUFLEN;
+
+
+        // Initialize Winsock
+        iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (iResult != 0) {
+            std::cout << "Client WSAStartup failed with error: \n" << iResult;
+            return;
+        }
+
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        // Resolve the server address and port
+        iResult = getaddrinfo(serverIP, DEFAULT_PORT, &hints, &result);
+        if (iResult != 0) {
+            std::cout << "Client getaddrinfo failed with error: \n" << iResult;
+            WSACleanup();
+            return;
+        }
+
+        // Attempt to connect to an address until one succeeds
+        for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+            // Create a SOCKET for connecting to server
+            ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+                ptr->ai_protocol);
+            if (ConnectSocket == INVALID_SOCKET) {
+                std::cout << "Client socket failed with error: %ld\n" << WSAGetLastError();
+                WSACleanup();
+                return;
+            }
+
+            // Connect to server.
+            iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+            if (iResult == SOCKET_ERROR) {
+                closesocket(ConnectSocket);
+                ConnectSocket = INVALID_SOCKET;
+                continue;
+            }
+            break;
+        }
+
+        freeaddrinfo(result);
+
+        if (ConnectSocket == INVALID_SOCKET) {
+            std::cout << "Client Unable to connect to server!\n";
+            WSACleanup();
+            return;
+        }
+
+        connected = true;
+    }
+
+    bool isConnected() {
+        return connected;
+    }
 
     void sendToServer(const char* message) {
         // Send an initial buffer
@@ -47,5 +134,9 @@ public:
         // cleanup
         closesocket(ConnectSocket);
         WSACleanup();
+    }
+
+    std::string* getLastMessage() {
+        return lastMessage;
     }
 };
--- GameServer.hpp
+++ GameServer.hpp
@@ -1,12 +1,125 @@
 #pragma once
+#undef UNICODE
 
-#include "Server.hpp"
+#define WIN32_LEAN_AND_MEAN
 
-class GameServer: public Server {
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+/**Creates a multithreading-only server which waits for a client to connect with him and send him some messages.
+**/
+
+class GameServer {
+protected:
+    int iResult;
+    int iSendResult;
+    char* recvbuf;
+    int recvbuflen;
+    SOCKET ClientSocket;
+    SOCKET ListenSocket;
+    std::string* lastMessage;
+    bool connected = false;
+
 public:
-    std::string* getLastMessage() {
-        return lastMessage;
+
+    GameServer() {
+        lastMessage = nullptr;
+        WSADATA wsaData;
+
+        ListenSocket = INVALID_SOCKET;
+        ClientSocket = INVALID_SOCKET;
+
+        struct addrinfo* result = NULL;
+        struct addrinfo hints;
+
+
+        recvbuf = new char[DEFAULT_BUFLEN];
+        recvbuflen = DEFAULT_BUFLEN;
+
+        // Initialize Winsock
+        iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (iResult != 0) {
+            std::cout << "Server WSAStartup failed with error: \n" << iResult;
+            return;
+        }
+
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        hints.ai_flags = AI_PASSIVE;
+
+        // Resolve the server address and port
+        iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+        if (iResult != 0) {
+            std::cout << "Server getaddrinfo failed with error: \n" << iResult;
+            WSACleanup();
+            return;
+        }
+
+        // Create a SOCKET for connecting to server
+        ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+        if (ListenSocket == INVALID_SOCKET) {
+            std::cout << "Server socket failed with error: %ld\n" << WSAGetLastError();
+            freeaddrinfo(result);
+            WSACleanup();
+            return;
+        }
+
+        // Setup the TCP listening socket
+        iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            std::cout << "Server bind failed with error: \n" << WSAGetLastError();
+            freeaddrinfo(result);
+            closesocket(ListenSocket);
+            WSACleanup();
+            return;
+        }
+
+        freeaddrinfo(result);
+
+        iResult = listen(ListenSocket, SOMAXCONN);
+        if (iResult == SOCKET_ERROR) {
+            std::cout << "Server listen failed with error: \n" << WSAGetLastError();
+            closesocket(ListenSocket);
+            WSACleanup();
+            return;
+        }
+
+        std::cout << "Server successfully set up.\n";
     }
+
+    void waitForClient() {
+        // Accept a client socket
+        ClientSocket = accept(ListenSocket, nullptr, nullptr);
+        if (ClientSocket == INVALID_SOCKET) {
+            std::cout << "Server accept failed with error: \n" << WSAGetLastError();
+            closesocket(ListenSocket);
+            WSACleanup();
+            return;
+        }
+
+        connected = true;
+        // No longer need server socket
+        closesocket(ListenSocket);
+
+        std::cout << "Server successfully connected to client. Ready to receive messages.\n";
+
+        this->receive();
+    }
+
+
 
     void sendToClient(const char* message) {
         iResult = send(ClientSocket, message, (int)strlen(message), 0);
@@ -26,10 +139,11 @@ public:
             //save message
             for (int i = 0; i < iResult; i++) {
                 lastMessage = new std::string();
+                char c = recvbuf[i];
                 lastMessage->push_back(recvbuf[i]);
             }
 
-            std::cout << "Server received message: " << *lastMessage;
+            std::cout << "Server received message: " << lastMessage->c_str();
             if (iResult < 0) {
                 std::cout << "Server recv failed with error: \n" << WSAGetLastError();
                 closesocket(ClientSocket);
@@ -49,5 +163,14 @@ public:
         // cleanup
         closesocket(ClientSocket);
         WSACleanup();
+    }
+
+
+    std::string* getLastMessage() {
+        return lastMessage;
+    }
+
+    bool isConnected() {
+        return connected;
     }
 };
