5c4302d6fdd5ec05e8374fe9d275c9032a11351c
Preacher <63048280+juliankoll@users.noreply.github.com>
2021-08-04 14:39:50 +0200

    yes2

--- ProjectileHandling.hpp
+++ ProjectileHandling.hpp
@@ -100,6 +100,7 @@ public:
 		int networkingStart = NetworkCommunication::getTokenCount();
 		NetworkCommunication::addToken(networkingStart);
 		int networkingEnd = networkingStart + (4 * newProjectiles->size());
+		if (newProjectiles->size() > 0) networkingEnd--;
 		NetworkCommunication::addToken(networkingEnd);
 		
 		for (int i = 0; i < newProjectiles->size(); i++) {
