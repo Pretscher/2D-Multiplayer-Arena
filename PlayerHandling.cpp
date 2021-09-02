9a7e06428316f35afae551378b3e3b63fb3de471
Preacher <63048280+juliankoll@users.noreply.github.com>
2021-09-02 22:46:43 +0200

    Revert + push of better pathfinding

--- PlayerHandling.cpp
+++ PlayerHandling.cpp
@@ -16,18 +16,18 @@ PlayerHandling::PlayerHandling() {//basically 100% hardcorded stuff for players
 	players = new Player * [playerCount];
 	for (int i = 0; i < playerCount; i++) {
 		int startRow = i * 1000 / playerCount + 200;//move players away from each other in row
-		players[i] = new Player(startCol, startRow, rectSize, rectSize, vel, defaultMaxHp, defaultDmg);//places players on map, col dist depends on playercount
+		players [i] = new Player(startCol, startRow, rectSize, rectSize, vel, defaultMaxHp, defaultDmg);//places players on map, col dist depends on playercount
 	}
 }
 
 void PlayerHandling::draw() {
 	for (int i = 0; i < playerCount; i++) {
-		if (players[i]->getHp() > 0) {
-			players[i]->draw();//if he has a path, he moves on this path
+		if (players [i]->getHp() > 0) {
+			players [i]->draw();//if he has a path, he moves on this path
 		}
 	}
 }
- 
+
 void PlayerHandling::sendPlayerData() {
 	int otherPlayer = 0;
 	if (myPlayerI == 0) {
@@ -35,7 +35,12 @@ void PlayerHandling::sendPlayerData() {
 	}
 
 	Player* me = players [myPlayerI];
-	if (me->hasNewPath == true) {
+	if (me->hasPath() == false) {
+		NetworkCommunication::addToken(0);//bool if path should be interrupted
+		NetworkCommunication::addToken(me->getRow());
+		NetworkCommunication::addToken(me->getCol());
+	}
+	else if (me->hasNewPath == true) {
 		me->hasNewPath = false;
 		NetworkCommunication::addToken(1);//bool if new bath was found
 
@@ -45,18 +50,9 @@ void PlayerHandling::sendPlayerData() {
 			NetworkCommunication::addToken(me->pathYpositions [i]);
 		}
 	}
-	else {
-		NetworkCommunication::addToken(0);
-
-		//no new path, so we dont know where the other player is if we just connected or had 
-		//a lag so long that we missed an entire path-walk => just exchange positions
-		
-		NetworkCommunication::addToken(me->getRow());
-		NetworkCommunication::addToken(me->getCol());
-	}
 
 
-	NetworkCommunication::addToken(players[otherPlayer]->getHp());
+	NetworkCommunication::addToken(players [otherPlayer]->getHp());
 }
 
 /** Has to pass pathfinding so that we can update pathfinding-graph if player positions changed
@@ -68,9 +64,21 @@ void PlayerHandling::receivePlayerData(Pathfinding* pathfinding) {
 		otherPlayer = 1;
 	}
 
-	if (NetworkCommunication::receiveNextToken() == 1) {//new path
+	int actionIndex = NetworkCommunication::receiveNextToken();
+
+	if (actionIndex == 0) {//interrupt path/no path is there
+		if (players [otherPlayer]->hasPath() == true) {
+			players [otherPlayer]->deletePath();
+
+			int row = NetworkCommunication::receiveNextToken();
+			int col = NetworkCommunication::receiveNextToken();
+			players [otherPlayer]->setRow(row);
+			players [otherPlayer]->setCol(col);
+		}
+	}
+	else if (actionIndex == 1) {//new path
 		int pathLenght = NetworkCommunication::receiveNextToken();
-		
+
 		int* pathX = new int [pathLenght];
 		int* pathY = new int [pathLenght];
 		for (int i = 0; i < pathLenght; i++) {
@@ -79,21 +87,11 @@ void PlayerHandling::receivePlayerData(Pathfinding* pathfinding) {
 		}
 		players [otherPlayer]->givePath(pathX, pathY, pathLenght);
 	}
-	else {
-		//no new path, so we dont know where the other player is if we just connected or had 
-		//a lag so long that we missed an entire path-walk => just exchange positions
-		int row = NetworkCommunication::receiveNextToken();
-		int col = NetworkCommunication::receiveNextToken();
-		if (players [otherPlayer]->hasPath() == false) {
-			players [otherPlayer]->setRow(row);
-			players [otherPlayer]->setCol(col);
-		}
-	}
 
 	int hp = NetworkCommunication::receiveNextToken();
 	hpSyncDelay ++;
-	if(hpSyncDelay > 10) {
-		players[myPlayerI]->setHp(hp);
+	if (hpSyncDelay > 10) {
+		players [myPlayerI]->setHp(hp);
 		hpSyncDelay = 0;
 	}
 }
